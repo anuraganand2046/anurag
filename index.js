45f2f777b7d34d9ae13367770191ca671a73de3c
Anurag Anand <61843778+anuraganand2046@users.noreply.github.com>
2020-12-15 14:11:48 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,6 +1,26 @@
-const fs= require("fs");
-fs.writeFileSync("read.txt", "Anurag Anand");
-fs.appendFileSync("read.txt", " Ritul Anand");
-const p= fs.readFileSync("read.txt");
-q= p.toString();
-console.log(q);
+const express= require('express');
+const hbs= require("hbs");
+const app= express();
+const path= require('path');
+const port= 8000;
+const staticpath= path.join(__dirname, "../public");
+const templatepath= path.join(__dirname, "../template/views");
+const partialspath= path.join(__dirname, "../template/partials");
+app.set('view engine', 'hbs');
+app.set("views", templatepath);
+hbs.registerPartials(partialspath);
+app.get("/", (req, res)=>{
+    res.render("index");
+});
+//app.use(express.static(staticpath));
+app.get("/", (req, res) => {
+    res.send("Anurag Anand sucks");
+});
+app.get("*", (req, res) => {
+    res.render("404", {
+        errorcomment: "Page not found",
+    });
+})
+app.listen(port, ()=>{
+    console.log(`listening to port ${port}`);
+});
